ac78d643c68f4ad79bfeb7df5d03b08e07c17f8e
reanrean <reanrean@users.noreply.github.com>
2016-02-19 18:23:26 +0800

    add function

--- maint.js
+++ maint.js
@@ -1,18 +1,34 @@
 var wname=[wardrobe,wardrobe_i,wardrobe_s];
 var wowner=['rean','ivangift','seal100x'];
 
+var rows=0;
+var field_desc=['名字','分类','编号','心级',
+	'华丽','简约','优雅','活泼','成熟','可爱','性感','清纯','清凉','保暖',
+	'tag','来源','套装'];
+
 function show(){
 	var pass='d0be2dc421be4fcd0172e5afceea3970e2f3d940';
-	var userInput=$("#passcode").val();
+	var userInput=$.sha1($("#passcode").val());
 	$("#passcode").val('');
-	userInput=$.sha1(userInput);
 	if (userInput==pass){
 		go();
 	}else{
 		$("#info").html('&#x1f64a&#x1f64a&#x1f64a&#x1f64a&#x1f64a');
 	}
 }
+
 function go(){
+	var menu='<table width=50%>';
+	var line=td('<a href="#" onclick=go_comp() ><b>Compare</b></a>');
+		line+=td('<a href="#" onclick=go_add() ><b>Add</b></a>');
+	menu+=tr(line);
+	$("#menu").html(menu);
+	$("#info").html('');
+	$("#extra").html('');
+}
+
+function go_comp(){
+	rows=0;
 	var cnt=[];
 	for (var j in wname){
 		cnt[j]=0;
@@ -31,7 +47,7 @@ function go(){
 	for (var i in wname){
 		str[i]=[];
 	}
-	for(var i=0;i<max;i++){
+	for(var i=0;i<max;i++){//assign values into str[] from wardrobe
 		for (var j in wname){
 			if(wname[j][i]){
 				str[j][i]=wname[j][i][0]+'/'+wname[j][i][1].substr(0,1);
@@ -45,36 +61,137 @@ function go(){
 		}
 	}
 	var out='<table>';
-	out+=td(list);
-	out+=td('<hr>');
-	for(var i=1;i<wname.length;i++){
-		out+=td("Extra values in "+wowner[0]+"'s wardrobe VS "+wowner[i]+"'s:");
-		out+=td(compare(str[0],str[i],max));
-		out+=td('<hr>');
-		out+=td("Extra values in "+wowner[i]+"'s wardrobe VS "+wowner[0]+"'s:");
-		out+=td(compare(str[i],str[0],max));
-		out+=td('<hr>');
+	out+=tr(td(list));
+	out+=tr(td('<hr>'));
+	for(var i=1;i<wname.length;i++){//compare [0] with others
+		out+=tr(td("Extra values in "+wowner[0]+"'s wardrobe VS "+wowner[i]+"'s:"));
+		out+=tr(td(compare(str[0],str[i],max,'<br/>')));
+		out+=tr(td('<hr>'));
+		out+=tr(td("Extra values in "+wowner[i]+"'s wardrobe VS "+wowner[0]+"'s:"));
+		out+=tr(td(compare(str[i],str[0],max,'<br/>')));
+		out+=tr(td('<hr>'));
 	}
 	out+='</table>';
 	
 	$("#info").html(out);
+	$("#extra").html('');
 }
 
-function compare(a,b,len){//a contains but b not
+function go_add(){
+	var form='<table id="go_add"><tbody>';
+	var line='';
+	for (var i in field_desc){
+		line+=td(field_desc[i]);
+	}
+	form+=tr(line);
+	form+='</tbody></table>';
+	
+	form+='<table>';
+	line=td(button('Add',"add()"));
+	line+=td(button('Del',"del()"));
+	line+=td(button('Validate',"validWardrobe()"));
+	line+=td(button('Generate',"genWardrobe()"));
+	form+=tr(line);
+	form+='</table>';
+	
+	$("#info").html(form);
+	
+	add();
+}
+
+function add(){
+	rows++;
+	var line='';
+	for (var i=1;i<=field_desc.length;i++){
+		line+=td('<input id="in'+rows+'_'+i+'" size="'+((i==1||i==2||i==15)?5:2)+'">');
+	}
+	$('#go_add > tbody:last-child').append(tr(line));
+	arrowKey();
+}
+
+function del(){
+	if(rows>1){
+		rows--;
+		$('#go_add tbody tr:last').remove();
+	}
+}
+
+function genWardrobe(){
 	var out='';
-	for(var i=0;i<len;i++){
+	for(var j=1;j<=rows;j++){
+		out+='  [';
+		for (var i=1;i<=17;i++){
+			out+="'"+$('#in'+j+'_'+i).val()+"'"+(i==17?'':',');
+		}
+		out+='],<br/>';
+	}
+	$("#extra").html(out);
+}
+
+function validWardrobe(){
+	var out='';
+	var extra='';
+	var chk1=[1,2,3,4,16];
+	var chk2=[6,8,10,12,14];
+	var prop=['SSS','SS','S','A','B','C',''];
+	for(var j=1;j<=rows;j++){
+		var head=(j==1? '':'<br/>')+'row'+j+'('+$('#in'+j+'_1').val()+'): '
+		var check=[];
+		var cont='';
+		for (var i=1;i<=field_desc.length;i++){
+			check[i]=$('#in'+j+'_'+i).val();
+			if((jQuery.inArray(i, chk1)>-1)&&(!check[i])) {cont+=field_desc[i-1]+'null, ';}
+			if(jQuery.inArray(i, chk2)>-1){
+				if(check[i]&&check[i*1-1]){cont+=field_desc[i-2]+field_desc[i-1]+'both, ';}
+				else if(!check[i]&&!check[i*1-1]){cont+=field_desc[i-2]+field_desc[i-1]+'null, ';}
+				else if(jQuery.inArray(check[i], prop)<0){cont+=field_desc[i-1]+'invalid, '}
+				else if(jQuery.inArray(check[i-1], prop)<0){cont+=field_desc[i-2]+'invalid, '}
+			}
+		}
+		if(cont){out+=head+cont;}
+	}
+	$("#extra").html(out);
+}
+
+function compare(a,b,len,split){//a contains but b not
+	var out='';
+	for(var i=0;i<len&&a[i];i++){
 		var ind=0;
-		for(var j=0;j<len;j++){
+		for(var j=0;j<len&&b[j];j++){
 			if (a[i]==b[j]){
 				ind=1;
 				break;
 			}
 		}
-		if(!ind) out+=a[i]+'</br>';
+		if(!ind) out+=a[i]+split;
 	}
 	return out;
 }
 
-function td(text){
-	return '<tr><td>'+text+'</td></tr>';
+function arrowKey() {
+$('input').keydown(function(e) {
+    if (e.keyCode==39) {
+		if(this.value.length==this.value.slice(0, this.selectionStart).length){
+        $(this).parent().next().find('input').focus();
+		}
+    }
+    if (e.keyCode==37) {
+        if(this.value.slice(0, this.selectionStart).length==0){
+        //$(this).prev('input').focus();
+        $(this).parent().prev().find('input').focus();
+		}
+    }
+});
+}
+
+function td(text,attr){
+	return '<td'+(attr? ' '+attr : '')+'>'+text+'</td>';
+}
+
+function tr(text,attr){
+	return '<tr'+(attr? ' '+attr : '')+'>'+text+'</tr>';
+}
+
+function button(text,onclick){
+	return '<button onclick="'+onclick+'">'+text+'</button>'
 }
