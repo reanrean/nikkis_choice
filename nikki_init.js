0d0b51dc3c6569187573a3e3f7709f125efbcca6
reanrean <reanrean@users.noreply.github.com>
2016-02-21 01:19:38 +0800

    upd 160221

--- nikki_init.js
+++ nikki_init.js
@@ -1,12 +1,4 @@
-function init() {
-	var mine = loadFromStorage();
-	calcDependencies();
-	drawFilter();
-	drawTheme();
-	drawImport();
-	switchCate(category[0]);
-	updateSize(mine);
-	refreshShoppingCart();
-	initEvent();
-	wardrobe_cnt();
-}
+$(document).ready(function () {
+	init();
+	menuFixed("clothes");
+});
