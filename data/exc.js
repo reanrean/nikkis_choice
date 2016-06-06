34dad46dafb93392018bccaaf252f8354618ce67
reanrean <reanrean@users.noreply.github.com>
2016-06-06 11:02:47 +0800

    Update exc.js

--- data/exc.js
+++ data/exc.js
@@ -1,10 +1,10 @@
-var wspos=6000;
+var wspos=4335;
 var wardrobe_a=wardrobe;
 var wardrobe=function() {
 	var ret = [];
 	for (var i in wardrobe_a) {
 		var tmp=wardrobe[i];
-		if(i>wspos) {continue;}
+		if(i>=wspos) {continue;}
 		ret.push(tmp);
 	}
 	return ret;
