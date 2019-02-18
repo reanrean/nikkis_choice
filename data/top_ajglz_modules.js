661dd8a98213d4abf542c310646b7fa680a043ca
reanrean <rinnerean@yahoo.co.jp>
2019-02-18 23:21:28 +0800

    2.17

--- data/top_ajglz_modules.js
+++ data/top_ajglz_modules.js
@@ -3,6 +3,7 @@ var auto_skip = ['不使用'];
 var modules_top = [
 //modulename, title, searchType, searchString, mode
 //searchType: 0-hardcode_name, 1-set
+['最新活动','上元灯花宴',1,'上元灯花宴',''],
 ['最新活动','相守之翼',1,'相守之翼',''],
 ['最新活动','倾心之羽',1,'倾心之羽',''],
 ['最新活动','猪猪包子铺',1,'猪猪包子铺',''],
